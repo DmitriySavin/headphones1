8f85080a29c90d437c77c18fcb6e336ab1612f7d
DmitriySavin <146013906+DmitriySavin@users.noreply.github.com>
2024-10-21 22:08:51 +0000

    Deploying to gh-pages from @ DmitriySavin/headphones1@37af917dc29f977fd0a332b605d4a6532de7d617 🚀

---
+++ index.eb03d30e.js
@@ -0,0 +1,2 @@
+document.getElementById("blackButton").addEventListener("click",(function(){document.getElementById("colorImage").src="src/images/headphones-black.jpg"})),document.getElementById("whiteButton").addEventListener("click",(function(){document.getElementById("colorImage").src="src/images/headphones-white.jpg"}));
+//# sourceMappingURL=index.eb03d30e.js.map
