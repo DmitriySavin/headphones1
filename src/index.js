eda3bcf404b016cc0878c95aa88c3157b845c3f7
DmitriySavin <debilv556@gmail.com>
2024-11-23 14:06:44 +0100

    фвв

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
 //         console.log('Ошибка загрузки изображения:', colorImage.src);
 //     };
 
-//  document.addEventListener('DOMContentLoaded', function() {
-//        document.getElementById('blackButton').addEventListener('click', function () {
-//            document.getElementById('colorImage').src = 'src/images/headphones-black.jpg';
-//        });
+// document.addEventListener('DOMContentLoaded'), function () {
+//     document.getElementById('blackButton').addEventListener('click', function () {
+//         document.getElementById('colorImage').src = 'src/images/headphones-black.jpg';
+//     });
 
-document.getElementById('whiteButton').addEventListener('click', function() {
-    document.getElementById('colorImage').src = '/images/headphones-white.jpg'; 
-});
+// document.getElementById('whiteButton').addEventListener('click', function() {
+//     document.getElementById('colorImage').src = '/images/headphones-white.jpg'; 
+// });
---
+++ src/js/purchase-img.js
@@ -0,0 +1,8 @@
+document.getElementById('blackButton').addEventListener('click', function () {
+    document.getElementById('colorImage').src = '/src/images/headphones-black.jpg'; // Укажите путь к черному изображению
+});
+
+
+document.getElementById('whiteButton').addEventListener('click', function() {
+    document.getElementById('colorImage').src = '/src/images/headphones-white.jpg'; // Укажите путь к белому изображению
+});
